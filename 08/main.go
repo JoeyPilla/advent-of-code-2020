ca2b2e7ec2e0f384a027108869f7845d038e793f
JoeyPilla <38769311+JoeyPilla@users.noreply.github.com>
2020-12-08 21:34:12 -0500

    refactored

--- 08/main.go
+++ 08/main.go
@@ -22,6 +22,7 @@ type Instruction struct {
 	op       Operation
 	arg      int
 	calledAt []int
+	err      bool
 }
 
 type Result struct {
@@ -29,23 +30,116 @@ type Result struct {
 	finished bool
 }
 
+type Program struct {
+	acc          int
+	done         chan bool
+	instrCount   int
+	instruction  *Instruction
+	instructions []Instruction
+	kill         chan bool
+	pc           int
+	results      chan Result
+	wg           *sync.WaitGroup
+}
+
+func CreateProgram(instructions []Instruction, results chan Result, kill chan bool, wg *sync.WaitGroup) Program {
+	return Program{
+		acc:          0,
+		done:         make(chan bool),
+		instrCount:   0,
+		instruction:  &Instruction{},
+		instructions: instructions,
+		kill:         kill,
+		pc:           0,
+		results:      results,
+		wg:           wg,
+	}
+}
+
+func (p *Program) Run() {
+	defer p.wg.Done()
+	for {
+		select {
+		case <-p.kill:
+			return
+		case <-p.done:
+			return
+		default:
+			p.readInstruction()
+		}
+	}
+}
+
+func (p *Program) getInstruction() *Instruction {
+	if p.pc > len(p.instructions) {
+		p.results <- Result{
+			acc:      p.acc,
+			finished: false,
+		}
+		return &Instruction{
+			err: true,
+		}
+	}
+	if p.pc == len(p.instructions) {
+		p.results <- Result{
+			acc:      p.acc,
+			finished: true,
+		}
+		return &Instruction{
+			err: true,
+		}
+	}
+	return &p.instructions[p.pc]
+}
+
+func (p *Program) readInstruction() {
+	p.instruction = p.getInstruction()
+	if p.instruction.err || len(p.instruction.calledAt) > 0 {
+		p.results <- Result{
+			acc:      p.acc,
+			finished: false,
+		}
+		close(p.done)
+		return
+	}
+	p.instruction.calledAt = append(p.instruction.calledAt, p.instrCount)
+	p.processInstruction()
+	p.instrCount += 1
+}
+
+func (p *Program) processInstruction() {
+	i := p.instruction
+	switch i.op {
+	case Acc:
+		p.acc += i.arg
+		p.pc += 1
+	case Jmp:
+		p.pc += i.arg
+	case Nop:
+		p.pc += 1
+	default:
+		fmt.Errorf("invalid operation")
+	}
+}
+
 func part1(inputs []string) {
 	var wg sync.WaitGroup
 	results := make(chan Result)
-	done := make(chan bool)
-	go readResults(results, false, done)
+	kill := make(chan bool)
+	go readResults(results, false, kill)
 	instructions := LoadInstructions(inputs)
 	wg.Add(1)
-	go ProcessInstructions(instructions, results, &wg, done)
+	p := CreateProgram(instructions, results, kill, &wg)
+	go p.Run()
 	wg.Wait()
 	close(results)
 }
 
 func part2(inputs []string) {
 	var wg sync.WaitGroup
-	results := make(chan Result)
-	done := make(chan bool)
-	go readResults(results, true, done)
+	results := make(chan Result, len(inputs))
+	kill := make(chan bool)
+	go readResults(results, true, kill)
 	for i := 0; i < len(inputs); i++ {
 		instructions := LoadInstructions(inputs)
 		changed := false
@@ -54,7 +148,8 @@ func part2(inputs []string) {
 			continue
 		}
 		wg.Add(1)
-		go ProcessInstructions(instructions, results, &wg, done)
+		p := CreateProgram(instructions, results, kill, &wg)
+		go p.Run()
 	}
 	wg.Wait()
 	close(results)
@@ -95,58 +190,6 @@ func LoadInstructions(inputs []string) []Instruction {
 		}
 	}
 	return instructions
-}
-
-func ProcessInstructions(instructions []Instruction, r chan Result, wg *sync.WaitGroup, done chan bool) {
-	acc, pos, count := 0, 0, 0
-	defer wg.Done()
-	for {
-		select {
-		case <-done:
-			return
-		default:
-			if pos > len(instructions) {
-				r <- Result{
-					acc:      acc,
-					finished: false,
-				}
-				return
-			}
-			if pos == len(instructions) {
-				r <- Result{
-					acc:      acc,
-					finished: true,
-				}
-				return
-			}
-			i := &instructions[pos]
-			if len(i.calledAt) > 0 {
-				r <- Result{
-					acc:      acc,
-					finished: false,
-				}
-				return
-			}
-			i.calledAt = append(i.calledAt, count)
-			pos, acc = ProcessInstruction(i, pos, acc)
-			count++
-		}
-	}
-}
-
-func ProcessInstruction(i *Instruction, pos, acc int) (int, int) {
-	switch i.op {
-	case Acc:
-		acc += i.arg
-		pos += 1
-	case Jmp:
-		pos += i.arg
-	case Nop:
-		pos += 1
-	default:
-		fmt.Errorf("invalid operation")
-	}
-	return pos, acc
 }
 
 func main() {
